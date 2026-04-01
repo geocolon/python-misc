b5bcbb8402ed29e863596d78d6ab45cec64e78da
George Colon <geocolon@gmail.com>
2026-04-01 14:05:13 -0700

    Save Solution for question #1

--- Misc.py
+++ Misc.py
@@ -1,8 +1,14 @@
 message = "Hello, World!"
 print(message)
 
-nums = [2, 7, 11, 15], target = 9
+nums = [2, 7, 11, 15]
+target = 9
 
+for x in range(len(nums)):
+    for y in range(x + 1, len(nums)):
+        if nums[x] + nums[y] == target:
+            print([x, y])
+print("Problem 1: Two Sum (LeetCode #1)**")
 # Given an array of integers nums and an integer target, return the indices of the two numbers that add up to target.
 # You may assume each input has exactly one solution, and you can't use the same element twice.
 # Example:
@@ -15,3 +21,10 @@ nums = [2, 7, 11, 15], target = 9
 # What approach are you thinking about?
 
 # Then write your solution. Go.
+
+#Problem 2: Valid Anagram (LeetCode #242)**
+
+# Given two strings `s` and `t`, return `True` if `t` is an anagram of `s`, `False` otherwise.
+# ```
+# Example 1: s = "anagram", t = "nagaram" -> True
+# Example 2: s = "rat", t = "car" -> False
